64623755b526e11d8e286fbd978e2b7c4da2d321
Madhan Kumar <118105894+000MK@users.noreply.github.com>
2026-03-18 19:42:21 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/madhan-codes-ai/',
+  base: '/madhan/',
   server: {
     host: "::",
     port: 8080,
